93560158a7cb679300eb056baf3153dc1bd30d46
pigpgw <ceh20002@naver.com>
2024-08-14 16:17:02 +0000

    comment: min_size 주석 추가 및 기본 최소값 업데이트

--- mm.c
+++ mm.c
@@ -225,20 +225,22 @@ static void *coalesce(void *bp)
 static void *best_fit(size_t asize) {
     void *bp;
     void *best_bp = NULL;
-    size_t min_size = (size_t) - 1;  // SIZE_MAX는 <stdint.h>에 정의된 size_t의 최대값
+    // 64비트 시스템에서 표현할 수 있는 가장 큰 값으로 초기화하여 첫 번째로 찾은 적합한 블록이
+    // 무조건 이 값보다 작게 되어 업데이트가 가능하게 함
+    size_t min_size = 18446744073709551615; 
 
-    // 힙을 순회하면서 가장 적합한 블록을 찾습니다.
+    // 힙을 순회하면서 가장 적합한 블록을 찾기
     for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
         if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
-            // 현재 블록이 요청 크기보다 크거나 같고, 지금까지 찾은 것 중 가장 작은 블록이라면
+            // 현재 블록이 요청 크기보다 크거나 같고, 지금까지 찾은 것 중 가장 작은 블록일때 업데이트
             if (GET_SIZE(HDRP(bp)) < min_size) {
                 min_size = GET_SIZE(HDRP(bp));
                 best_bp = bp;
             }
         }
     }
-
-    return best_bp;  // 가장 적합한 블록을 반환하거나, 없으면 NULL 반환
+    // best_bp를 NULL로 초기에 선언해서 찾지 못한 상태라면 NULL반환
+    return best_bp;
 }
 
 static void place(void *bp, size_t asize){
